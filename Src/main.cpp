8abc8361dbdad61042ac96c9a1946c54d868d1b0
Owo uwu <owo@uwu.pm>
2024-02-21 17:27:23 +0100

    test

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,26 +1,25 @@
 #include <iostream>
 #include "LibUwu.h"
 
-int main(int argc, char** argv) {
-
-    // We have arguments. Uwwuifie these instead
+int main(int argc, char** argv)
+{
+    // We have arguments. Uwwuify these instead
     if (argc > 1)
     {
         // We have to put the args together first, because some replace-rules cross word-borders
-        std::stringstream  ss;
-        for (std::size_t i = 1; i < argc; i++)
+        std::stringstream ss;
+        for (std::size_t i = 1; i < argc; i++) {
             ss << std::string(argv[i]) + " ";
+        }
 
         std::cout << MakeUwu(ss.str()) << std::endl;
     }
-
     // Else, be prepared to get __piped__
     else
     {
         std::string buf;
         while (std::getline(std::cin, buf)) {
-            std::cout << MakeUwu(buf) << std::endl;
-            std::cout.flush();
+            std::cout << buf << std::endl;
         }
     }
 

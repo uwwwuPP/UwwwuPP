39f8a1909a2e22bd99915e862b3baedb2ea3261b
Owo uwu <owo@uwu.pm>
2024-02-21 11:14:21 +0100

    big impwuvments t-to uwuifiwation

--- Src/LibUwu.h
+++ Src/LibUwu.h
@@ -32,6 +32,66 @@ static inline auto ValidatorFindingIsCompleteWord(const std::string& original, c
         return false;
 }
 
+// This validator will only replace findings 75% of the time
+static inline auto Validator45Percent(const std::string& original, const std::string& finding, const std::size_t index) -> bool {
+    // Seed rng based on string
+    std::mt19937 rng(std::hash<std::string>()(original)); 
+
+    // Replace at 75% chance
+    return (rng() % 4) < 3;
+}
+
+// This validator will only replace findings, if they are a complete word, and not just part of a word, at 75%
+static inline auto ValidatorFindingIsCompleteWordAt75Percent(const std::string& original, const std::string& finding, const std::size_t index) -> bool {
+    return Validator45Percent(original, finding, index) && ValidatorFindingIsCompleteWord(original, finding, index);
+}
+
+// Validator for stuttering (y -> y-y)
+// but only sometimes (random change),
+// and if it is the first character of a word,
+// DISABLED: and if it is followed by a vowel
+static inline auto ValidatorStutter(const std::string& original, const std::string& finding, const std::size_t index) -> bool {
+    // Don't replace, if we're at the end of our string
+    if (index + finding.length() == original.length())
+        return false;
+
+    // This is a bit tricky, because we can't abort if we're on the first char
+    // but we can can't not check the previous char either. Running big risk
+    // of causing a segfault. So we have to not check the previous character,
+    // if we are on the first char.
+
+    // Are we on the first char?
+    const bool isFirstChar = index == 0;
+
+    // Fetch the last character
+    const char lastChar = isFirstChar ? '\0' : CharTools::MakeLower(original[index - 1]);
+
+    // Fetch the next character
+    const char nextChar = CharTools::MakeLower(original[index + finding.length()]);
+
+    // Don't replace, if the last char is a letter
+    if ((!isFirstChar) && (CharTools::IsLetter(lastChar)))
+        return false;
+
+    // Don't replace, if the next char is not a vowel
+    /*
+    if (!CharTools::IsVowel(nextChar))
+        return false;
+    */
+
+    // Initialize deterministic prng
+    // Can't initialize one for all scopes, they don't seem to play
+    // nicely with being passed to lambda functions...
+    static std::mt19937 rng(std::hash<std::string>()(original)); // Seed rng based on string
+
+    // Chance (out of a 100) for the mutation to take place
+    // if all preconditions are met
+    constexpr int chance = 25;
+
+    // Roll the dice, baby!
+    return (rng() % 100) < chance;
+}
+
 
 //! Will make a boring string look sooper dooper kawaii and cute :3
 static inline std::string MakeUwu(std::string boringString) {
@@ -39,7 +99,6 @@ static inline std::string MakeUwu(std::string boringString) {
     // none, lol
 
     // Slightly more complex... Multichar replacements, but we have to keep capitalization...
-    boringString = Util::ConditionalReplaceButKeepSigns(boringString, "th", "tw");
     boringString = Util::ConditionalReplaceButKeepSigns(boringString, "ove", "uv");
     boringString = Util::ConditionalReplaceButKeepSigns(boringString, "have", "haf");
     boringString = Util::ConditionalReplaceButKeepSigns(boringString, "tr", "tw");
@@ -54,11 +113,13 @@ static inline std::string MakeUwu(std::string boringString) {
     boringString = Util::ConditionalReplaceButKeepSigns(boringString, "twanks", "you're twe best :33 xoxo", ValidatorFindingIsCompleteWord);
     boringString = Util::ConditionalReplaceButKeepSigns(boringString, "hello", "hiiiiiii", ValidatorFindingIsCompleteWord);
     boringString = Util::ConditionalReplaceButKeepSigns(boringString, "dear", "hiiiiiii", ValidatorFindingIsCompleteWord);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, "hi", "hiiiiiii", ValidatorFindingIsCompleteWord);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, "come", "cum", ValidatorFindingIsCompleteWord);
 
     // Let's extend some phonetics
-    boringString = Util::ConditionalReplaceButKeepSigns(boringString, "hi", "hiiiiiii");
-    boringString = Util::ConditionalReplaceButKeepSigns(boringString, "ay", "aaay");
-    boringString = Util::ConditionalReplaceButKeepSigns(boringString, "ey", "eeey");
+    // These are quite agressive, so don't do them _every time_
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, "ay", "aaay", Validator45Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, "ey", "eeey", Validator45Percent);
 
     // Replace N with Ny, but only if succeeded by a vowel, and not (preceded by an o and succeeded by an "e{nonletter}"): "one" has such a niche pronunciation...
     boringString = Util::ConditionalReplaceButKeepSigns(
@@ -226,6 +287,24 @@ static inline std::string MakeUwu(std::string boringString) {
             }
     );
 
+    // Replace hi with hiiiiiii, but only if it's the last two letters of a word
+    boringString = Util::ConditionalReplaceButKeepSigns(
+            boringString,
+            "hi",
+            "hiiiiiii",
+            [](const std::string& original, const std::string& finding, const std::size_t index) {
+                // Replace if we're at the end of this line/segment
+                if (index + finding.length() == original.length())
+                    return true;
+
+                // Fetch the next char
+                const char nextChar = CharTools::MakeLower(original[index + finding.length()]);
+
+                // Replace if the next char is not a letter
+                return !CharTools::IsLetter(nextChar);
+            }
+    );
+
     // Replace R with W, but only (if it's preceeded by a vowel,
     // or preceeded by another 'r',
     // or if it's the first character of a word)
@@ -266,53 +345,16 @@ static inline std::string MakeUwu(std::string boringString) {
             }
     );
 
-    // Replace y with y-y (imitates shy stuttering), but only sometimes (random change),
-    // and if it is the first character of a word,
-    // and if it is followed by a vowel
-    boringString = Util::ConditionalReplaceButKeepSigns(
-            boringString,
-            "y",
-            "y-y",
-            [boringString](const std::string& original, const std::string& finding, const std::size_t index) {
-                // Don't replace, if we're at the end of our string
-                if (index + finding.length() == original.length())
-                    return false;
+    // Replace y with for example y-y (imitates shy stuttering)
+    for (char c = 'a'; c <= 'z'; c++) {
+        std::stringstream ss;
+        ss << c;
+        const std::string find = ss.str();    // "y"
+        ss << '-' << c;
+        const std::string replace = ss.str(); // "y-y"
 
-                // This is a bit tricky, because we can't abort if we're on the first char
-                // but we can can't not check the previous char either. Running big risk
-                // of causing a segfault. So we have to not check the previous character,
-                // if we are on the first char.
-
-                // Are we on the first char?
-                const bool isFirstChar = index == 0;
-
-                // Fetch the last character
-                const char lastChar = isFirstChar ? '\0' : CharTools::MakeLower(original[index - 1]);
-
-                // Fetch the next character
-                const char nextChar = CharTools::MakeLower(original[index + finding.length()]);
-
-                // Don't replace, if the last char is a letter
-                if ((!isFirstChar) && (CharTools::IsLetter(lastChar)))
-                    return false;
-
-                // Don't replace, if the next char is not a vowel
-                if (!CharTools::IsVowel(nextChar))
-                    return false;
-
-                // Initialize deterministic prng
-                // Can't initialize one for all scopes, they don't seem to play
-                // nicely with being passed to lambda functions...
-                static std::mt19937 rng(std::hash<std::string>()(boringString)); // Seed rng based on string
-
-                // Chance (out of a 100) for the mutation to take place
-                // if all preconditions are met
-                constexpr int chance = 40;
-
-                // Roll the dice, baby!
-                return (rng() % 100) < chance;
-            }
-    );
+        boringString = Util::ConditionalReplaceButKeepSigns(boringString, find, replace, ValidatorStutter);
+    }
 
     // Replace random punctuation with uwwwwu cute symbols
     // About evewy fifteenth symbol
@@ -328,7 +370,7 @@ static inline std::string MakeUwu(std::string boringString) {
         }
         else if ((c == '!') && (rng() % 15 == 0))
         {
-            ss << "!! Thadws impowtant! <3 ";
+            ss << "!! thadws impowtant! <3 ";
         }
         else if ((c == ',') && (rng() % 15 == 0))
         {
@@ -336,7 +378,7 @@ static inline std::string MakeUwu(std::string boringString) {
         }
         else if ((c == '?') && (rng() % 15 == 0))
         {
-            ss << "?? now tell me! >:( ";
+            ss << "?? nyow tell me! >.< ";
         }
         else
             ss << c;
@@ -344,10 +386,24 @@ static inline std::string MakeUwu(std::string boringString) {
     boringString = ss.str();
 
     // Also replace some ascii-"emojis'
-    boringString = StringTools::Replace(boringString, ":)", "UwU :D");
-    boringString = StringTools::Replace(boringString, ":D", ":3");
-    boringString = StringTools::Replace(boringString, ":-)", "UwwwU :3");
-    boringString = StringTools::Replace(boringString, "^^", "^.^ UwU");
+    boringString = StringTools::Replace(boringString, "^^", "^.^");
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":)", ":3", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":)", "x3", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":)", ">.<", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":)", ">///<", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":)", "^.^", ValidatorFindingIsCompleteWordAt75Percent);
+
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":-)", ":3", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":-)", "x3", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":-)", ">.<", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":-)", ">///<", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":-)", "^.^", ValidatorFindingIsCompleteWordAt75Percent);
+
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":D", ":3", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":D", "x3", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":D", ">.<", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":D", ">///<", ValidatorFindingIsCompleteWordAt75Percent);
+    boringString = Util::ConditionalReplaceButKeepSigns(boringString, ":D", "^.^", ValidatorFindingIsCompleteWordAt75Percent);
 
     // Some language replacement should happen after these more complex rules
     boringString = Util::ConditionalReplaceButKeepSigns(boringString, "c++", "c++ (rust is hella cutewr btw ^^)");
